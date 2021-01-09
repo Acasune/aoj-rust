a12d872e5109ace074b36e1cb48adda1200ceff2
Acasune <tttt4aaaa@gmail.com>
2021-01-09 19:57:34 +0900

    [solve] itp1-1-b

---
+++ ITP1/ITP1-1/itp1-1-b.rs
@@ -0,0 +1,14 @@
+﻿use std::io;
+
+fn main() {
+    let n = input();
+    let cube = n * n * n;
+    println!("{}", cube);
+}
+
+fn input() -> i32 {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("");
+
+    n.trim().parse().unwrap()
+}
