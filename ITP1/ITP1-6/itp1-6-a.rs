b2f0e9ba4d6d3129aabc478cb33446aee7106801
Acasune <tttt4aaaa@gmail.com>
2021-01-10 18:17:33 +0900

    [Solve] itp1-6-a

---
+++ ITP1/ITP1-6/itp1-6-a.rs
@@ -0,0 +1,9 @@
+﻿use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let stdin = io::stdin();
+    let line = stdin.lock().lines().nth(1).unwrap().unwrap();
+    let s = line.split_whitespace().rev().collect::<Vec<_>>().join(" ");
+    println!("{}", s);
+}
