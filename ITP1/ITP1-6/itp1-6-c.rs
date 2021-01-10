5097bb7a169615f00b1918969aa048274205ccf6
Acasune <tttt4aaaa@gmail.com>
2021-01-10 21:18:18 +0900

    [Solve] itp1-6-c

---
+++ ITP1/ITP1-6/itp1-6-c.rs
@@ -0,0 +1,36 @@
+﻿use std::{io::*, usize};
+
+fn main() {
+  let stdin = stdin();
+  let mut lines = stdin.lock().lines();
+  let N = lines.next().unwrap().unwrap().parse::<u64>().unwrap();
+
+  let mut buildings = vec![vec![vec![0; 10];3]; 4];
+
+  for i in 0..N {
+    let line = lines.next().unwrap().unwrap();
+    let mut args = line.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+    let b = args.next().unwrap() as usize;
+    let f = args.next().unwrap() as usize;
+    let r = args.next().unwrap() as usize;
+    let v = args.next().unwrap();
+
+    buildings[b-1][f-1][r-1] += v;
+
+  }
+
+  for b in 0..4 {
+    for f in 0..3 {
+      for r in 0..10 {
+        print!(" {}", buildings[b][f][r]);
+      }
+      println!("");
+    }
+    if b != 3 {
+      println!("####################");
+    }
+
+  }
+
+
+}
