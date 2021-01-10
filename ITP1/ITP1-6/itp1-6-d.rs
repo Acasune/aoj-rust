b152c26aa08be58c6b46e1f3b3d69c06d507e561
Acasune <tttt4aaaa@gmail.com>
2021-01-10 21:32:02 +0900

    [Solve] itp1-6-d

---
+++ ITP1/ITP1-6/itp1-6-d.rs
@@ -0,0 +1,30 @@
+﻿fn main() {
+  let mut s = String::new();
+  std::io::stdin().read_line(&mut s).ok();
+  let vec: Vec<usize> = s.split_whitespace().map(|a| a.parse().unwrap()).collect();
+  let N = vec[0];
+  let M = vec[1];
+  let mut a: Vec<Vec<i32>> = Vec::new();
+  let mut b: Vec<i32> = Vec::new();
+  for _ in 0..N {
+      let mut s = String::new();
+      std::io::stdin().read_line(&mut s).ok();
+      let row: Vec<i32> = s.split_whitespace().map(|a| a.parse().unwrap()).collect();
+      a.push(row);
+  }
+  for _ in 0..M {
+      let mut s = String::new();
+      std::io::stdin().read_line(&mut s).ok();
+      let v: i32 = s.trim().parse().unwrap();
+      b.push(v);
+  }
+  let mut c: Vec<i32> = vec![0; N];
+  for i in 0..N {
+      for j in 0..M {
+          c[i] += a[i][j] * b[j];
+      }
+  }
+  for i in 0..N {
+      println!("{}", c[i]);
+  }
+}
