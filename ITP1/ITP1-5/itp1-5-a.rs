bef5e6cc335820f8eeb60b79dbd0afdb6d01a034
Acasune <tttt4aaaa@gmail.com>
2021-01-10 16:01:20 +0900

    [Solve] itp1-5-1

---
+++ ITP1/ITP1-5/itp1-5-a.rs
@@ -0,0 +1,84 @@
+﻿use std::io::{stdout, BufWriter, Write};
+
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+      let mut iter = $s.split_whitespace();
+      let mut next = || { iter.next().unwrap() };
+      input_inner!{next, $($r)*}
+  };
+  ($($r:tt)*) => {
+      let stdin = std::io::stdin();
+      let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+      let mut next = move || -> String{
+          bytes
+              .by_ref()
+              .map(|r|r.unwrap() as char)
+              .skip_while(|c|c.is_whitespace())
+              .take_while(|c|!c.is_whitespace())
+              .collect()
+      };
+      input_inner!{next, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($next:expr) => {};
+  ($next:expr, ) => {};
+
+  ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+      let $var = read_value!($next, $t);
+      input_inner!{$next $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($next:expr, ( $($t:tt),* )) => {
+      ( $(read_value!($next, $t)),* )
+  };
+
+  ($next:expr, [ $t:tt ; $len:expr ]) => {
+      (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+  };
+
+  ($next:expr, chars) => {
+      read_value!($next, String).chars().collect::<Vec<char>>()
+  };
+
+  ($next:expr, usize1) => {
+      read_value!($next, usize) - 1
+  };
+
+  ($next:expr, $t:ty) => {
+      $next().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  solve();
+}
+
+fn solve() {
+  for i in 0.. {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut iter = buf.split_whitespace();
+    let h: i64 = iter.next().unwrap().parse().unwrap();
+    let w: i64 = iter.next().unwrap().parse().unwrap();
+
+    if h==0 && w==0{
+      break;
+    };
+
+    let mut s = String::new();
+    let mut rect = String::new();
+    for j in 0..w {
+      s.push('#');
+    }
+
+    for j in 0..h {
+      rect=rect+&s+"\n";
+    }
+    print!("{}\n",rect);
+  }
+}
