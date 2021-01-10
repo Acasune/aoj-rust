d87872d438010e529044455526e0ea8193295cef
Acasune <tttt4aaaa@gmail.com>
2021-01-11 01:39:10 +0900

    [Solve] itp1-8-a

---
+++ ITP1/ITP1-8/itp1-8-a.rs
@@ -0,0 +1,17 @@
+﻿use std::io;
+
+fn main() {
+  let mut s = String::new();
+  io::stdin().read_line(&mut s).expect("");
+
+  for c in s.chars() {
+    let x = match c {
+      t @ 'a'..='z' => t.to_uppercase().to_string(),
+      t @ 'A'..='Z' => t.to_lowercase().to_string(),
+      t  => t.to_string(),
+    };
+
+    print!("{}", x);
+    
+  }
+}
