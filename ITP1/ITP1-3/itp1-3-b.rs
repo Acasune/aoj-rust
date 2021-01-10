67eee41f4dae71d708913e7c05dc3d73ed9dafaf
Acasune <tttt4aaaa@gmail.com>
2021-01-10 09:33:39 +0900

    [Solve] itp1-3-b

---
+++ ITP1/ITP1-3/itp1-3-b.rs
@@ -0,0 +1,11 @@
+﻿fn main() {
+  for i in 1.. {
+      let mut x = String::new();
+      std::io::stdin().read_line(&mut x).unwrap();
+      let x: i32 = x.trim().parse().unwrap();
+      if x == 0 {
+          break;
+      }
+      println!("Case {}: {}", i, x);
+  }
+}
