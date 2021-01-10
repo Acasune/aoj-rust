9d5641c98e0a144f9b4ea585db9e44529e89f77e
Acasune <tttt4aaaa@gmail.com>
2021-01-11 01:55:24 +0900

    [Solve] itp1-8-b

---
+++ ITP1/ITP1-8/itp1-8-b.rs
@@ -0,0 +1,23 @@
+﻿use std::io;
+
+fn main() {
+  loop {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("");
+    if s == "0\n" {
+      break;
+    }
+
+    let mut ret = 0;
+
+    for c in s.chars() {
+      let x = match c {
+        t @ '0'..='9' => t as i32 - '0' as i32,
+        t => 0
+      };
+      ret += x;
+    };
+
+    println!("{}", ret);
+  }
+}
