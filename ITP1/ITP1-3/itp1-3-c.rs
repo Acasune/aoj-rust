0ef6b2a404537de4bf57d504c5dedc4093df3ef0
Acasune <tttt4aaaa@gmail.com>
2021-01-10 09:40:13 +0900

    [Solve] itp1-3-c

---
+++ ITP1/ITP1-3/itp1-3-c.rs
@@ -0,0 +1,15 @@
+﻿fn main() {
+  for i in 1.. {
+      let mut buf = String::new();
+      std::io::stdin().read_line(&mut buf).unwrap();
+
+      let mut iter = buf.split_whitespace();
+      let x: usize = iter.next().unwrap().parse().unwrap();
+      let y: usize = iter.next().unwrap().parse().unwrap();
+
+      if x == 0 && y == 0 {
+          break;
+      }
+      println!("{} {}", std::cmp::min(x,y), std::cmp::max(x,y));
+  }
+}
