a9a08c3072b2059da2ea3584257d10ddf0081583
Acasune <tttt4aaaa@gmail.com>
2021-01-11 02:46:16 +0900

    [Solve] itp1-8-c

---
+++ ITP1/ITP1-8/itp1-8-c.rs
@@ -0,0 +1,16 @@
+﻿use std::{io::Read, usize};
+
+fn main() {
+  let mut s = String::new();
+  std::io::stdin().read_to_string(&mut s).unwrap();
+  let mut v = vec![0; 256];
+
+  for c in s.bytes().map(|c| c as usize) {
+    v[c] += 1;
+  };
+
+  for c  in b'a'..b'z'+1 {
+    println!("{} : {}", c as char, (v[c as usize]+v[c as usize ^ 32]));
+  }
+
+}
