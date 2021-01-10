89344587b40cef5bd8fc0c09bd757d69d3b14e0e
Acasune <tttt4aaaa@gmail.com>
2021-01-10 17:02:47 +0900

    [Solve] itp1-5-c

---
+++ ITP1/ITP1-5/itp1-5-c.rs
@@ -0,0 +1,38 @@
+﻿
+fn main() {
+  solve();
+}
+
+fn solve() {
+  loop {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let mut iter = buf.split_whitespace();
+    let h: usize = iter.next().unwrap().parse().unwrap();
+    let w: usize = iter.next().unwrap().parse().unwrap();
+
+    if h==0 && w==0{
+      break;
+    };
+    draw_rect(h, w);
+    print!("\n")
+
+  }
+}
+
+fn draw_rect(H: usize, W: usize){
+  for h in 0..H {
+    for w in 0..W{
+      let dot =
+        if (w + h) % 2 ==0 {
+          "#"
+        }
+        else {
+          "."
+        };
+        print!("{}", dot);
+      }
+    print!("\n");
+  }
+}
