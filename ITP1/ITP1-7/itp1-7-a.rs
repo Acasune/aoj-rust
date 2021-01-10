4a5614812a575033778772cf2e7d59fc470d1834
Acasune <tttt4aaaa@gmail.com>
2021-01-10 22:00:06 +0900

    [Solve] itp1-7-a

---
+++ ITP1/ITP1-7/itp1-7-a.rs
@@ -0,0 +1,34 @@
+﻿use std::{io::*, usize};
+
+fn main() {
+    let stdin = stdin();
+    let mut lines = stdin.lock().lines();
+
+    loop {
+      let line = lines.next().unwrap().unwrap();
+      let mut args = line.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+      let m = args.next().unwrap();
+      let f = args.next().unwrap();
+      let r = args.next().unwrap();
+  
+      if m == -1 && f == -1 {
+          break;
+      }
+
+      if m * f < 0 {
+          println!("F");
+      } else if m + f >= 80 {
+          println!("A");
+      } else if m + f >= 65 {
+        println!("B");
+      } else if m + f >= 50 {
+        println!("C");
+      } else if m + f >= 50 || r >= 50 {
+        println!("C");
+      } else if m + f >= 30 {
+        println!("D");
+      } else {
+        println!("F");
+      }
+    }
+}
