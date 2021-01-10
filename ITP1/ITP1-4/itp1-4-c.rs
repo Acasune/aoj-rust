efb35d57acfa7fbc4553974cff8f7b5c68a13af4
Acasune <tttt4aaaa@gmail.com>
2021-01-10 10:46:01 +0900

    [Solve] itp1-4-c

---
+++ ITP1/ITP1-4/itp1-4-c.rs
@@ -0,0 +1,23 @@
+﻿fn main() {
+  for i in 1.. {
+      let mut buf = String::new();
+      std::io::stdin().read_line(&mut buf).unwrap();
+
+      let mut iter = buf.split_whitespace();
+      let x: i64 = iter.next().unwrap().parse().unwrap();
+      let op: char = iter.next().unwrap().parse().unwrap();
+      let y: i64 = iter.next().unwrap().parse().unwrap();
+
+
+      if op == '?' {
+          break;
+      }
+      match op {
+        '+' => println!("{}",x + y),
+        '-' => println!("{}",x - y),
+        '*' => println!("{}",x * y),
+        '/' => println!("{}",x / y),
+        _ => print!("Never happen")
+      }
+  }
+}
