6d73f14d77d448188afc2837ed61e50f06ee1c0f
Acasune <tttt4aaaa@gmail.com>
2021-01-11 10:12:48 +0900

    [Solve] itp1-9-a

---
+++ ITP1/ITP1-9/itp1-9-a.rs
@@ -0,0 +1,28 @@
+﻿use std::io;
+
+fn main() {
+  let mut W = String::new();
+  io::stdin().read_line(&mut W).expect("");
+  let W = W.trim().to_lowercase();
+
+  let mut cnt =0;
+
+  loop {
+    let mut S = String::new();
+    std::io::stdin().read_line(&mut S).ok();
+    let mut S = S.trim();
+    if S == "END_OF_TEXT" {
+      break;
+    }
+
+    let v: Vec<&str> = S.split(" ").collect();
+    for w in v {
+      if W == w.to_lowercase() {
+        cnt += 1;
+      }
+    }
+
+  }
+    println!("{}", cnt);
+
+}
