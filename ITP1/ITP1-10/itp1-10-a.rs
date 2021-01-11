a1fa96d21728739a8793a41ef719a405a59eedd7
Acasune <tttt4aaaa@gmail.com>
2021-01-11 12:27:10 +0900

    [Solve] itp1-10-a

---
+++ ITP1/ITP1-10/itp1-10-a.rs
@@ -0,0 +1,12 @@
+﻿fn main() {
+  let mut line = String::new();
+  std::io::stdin().read_line(&mut line).ok();
+  let mut iter = line.split_whitespace();
+  let a  = iter.next().unwrap().parse::<f64>().unwrap();
+  let b  = iter.next().unwrap().parse::<f64>().unwrap();
+  let c  = iter.next().unwrap().parse::<f64>().unwrap();
+  let d  = iter.next().unwrap().parse::<f64>().unwrap();
+
+  println!("{:.8}", ((b-d)*(b-d)+(c-a)*(c-a)).powf(0.5));
+
+}
