11e5779ff375a354a6586c2ea17a0c67f534c6dc
Acasune <tttt4aaaa@gmail.com>
2021-01-11 10:36:33 +0900

    [Solve] itp1-9-b

---
+++ ITP1/ITP1-9/itp1-9-b.rs
@@ -0,0 +1,26 @@
+﻿use std::io;
+
+fn main() {
+  loop {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    let mut s = s.trim().to_string();
+    if s == "-" {
+      break;
+    }
+
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).ok();
+    let n: usize = n.trim().parse().unwrap();
+
+    for _ in 0..n {
+        let mut h = String::new();
+        std::io::stdin().read_line(&mut h).ok();
+        let h: usize = h.trim().parse().unwrap();
+        s = format!("{}{}", &s[h..], &s[..h]);
+    }
+    println!("{}", s);
+
+  }
+
+}
