2a776c6e43829bf719a7630d3b1191702aff4430
Acasune <tttt4aaaa@gmail.com>
2021-01-11 13:00:10 +0900

    [Solve] itp1-10-b

---
+++ ITP1/ITP1-10/itp1-10-b.rs
@@ -0,0 +1,21 @@
+﻿fn main() {
+
+  let pi = std::f64::consts::PI;
+
+  let mut line = String::new();
+  std::io::stdin().read_line(&mut line).ok();
+  let mut iter = line.split_whitespace();
+  let a  = iter.next().unwrap().parse::<f64>().unwrap();
+  let b  = iter.next().unwrap().parse::<f64>().unwrap();
+  let c  = iter.next().unwrap().parse::<f64>().unwrap().to_radians();
+
+  let s = 0.5 * a * b * c.sin();
+  let h = b * c.sin();
+  let s = a * h / 2.;
+  let l = a + b + ((a - b * c.cos()).powi(2) + h.powi(2)).sqrt();
+
+  println!("{:.12}", s);
+  println!("{:.12}", l);
+  println!("{:.12}", h);
+
+}
