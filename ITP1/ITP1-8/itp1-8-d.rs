1d1720be9a1d2f62d29a0fd9b2f0f39d524da88a
Acasune <tttt4aaaa@gmail.com>
2021-01-11 09:39:06 +0900

    [Solve] itp1-8-d

---
+++ ITP1/ITP1-8/itp1-8-d.rs
@@ -0,0 +1,18 @@
+﻿fn main() {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    let s = s.trim();
+    let mut p = String::new();
+    std::io::stdin().read_line(&mut p).unwrap();
+    let p = p.trim();
+    let ss = format!("{}{}", s, s);
+
+    for i in 0..s.len() {
+        if p == &ss[i..i + p.len()]{
+            println!("Yes");
+            return ;
+        }
+    }
+    println!("No");
+
+}
