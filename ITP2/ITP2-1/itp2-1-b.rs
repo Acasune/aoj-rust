c997e73af5fac15f5f33da8de589594b23e191f3
Acasune <tttt4aaaa@gmail.com>
2021-01-16 23:05:46 +0900

    [Solve] itp2-1-b

---
+++ ITP2/ITP2-1/itp2-1-b.rs
@@ -0,0 +1,83 @@
+﻿use std::io::*;
+use std::str::FromStr;
+use std::{collections::*, usize};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let q: i64 = sc.get();
+    let mut que: VecDeque<i64> = VecDeque::new();
+
+    for i in 0..q {
+        sc.new_line();
+        let input = sc.get_as_vec::<i64>();
+        if input[0] == 0 {
+            match input[1] {
+                0 => que.push_front(input[2]),
+                1 => que.push_back(input[2]),
+                _ => {}
+            }
+        } else if input[0] == 1 {
+            println!("{}", que[input[1] as usize]);
+        } else {
+            match input[1] {
+                0 => {
+                    que.pop_front();
+                }
+                1 => {
+                    que.pop_back();
+                }
+                _ => {}
+            }
+        }
+    }
+}
