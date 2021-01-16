e777c9210fa717b521b7597f70950f2f7a39c5da
Acasune <tttt4aaaa@gmail.com>
2021-01-16 20:05:37 +0900

    [Solve] itp2-1-a

--- ITP2/ITP2-1/itp2-1-a.rs
+++ ITP2/ITP2-1/itp2-1-a.rs
@@ -1,3 +1,70 @@
-﻿fn main() {
-  println!("Hello World")
+﻿use std::io::*;
+use std::str::FromStr;
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n: i64 = sc.get();
+    let mut v: Vec<i64> = Vec::new();
+
+    for i in 0..n {
+        sc.new_line();
+        let input = sc.get_as_vec::<i64>();
+        if input[0] == 0 {
+            v.push(input[1]);
+        } else if input[0] == 1 {
+            println!("{}", v[input[1] as usize]);
+        } else {
+            v.pop();
+        }
+    }
 }
