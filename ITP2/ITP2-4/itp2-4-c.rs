d1c947b85f75c5d8ba3e9135f4c525bc7ee7bf74
Acasune <tttt4aaaa@gmail.com>
2021-01-18 21:59:27 +0900

    [Solve] itp2-4-c

---
+++ ITP2/ITP2-4/itp2-4-c.rs
@@ -0,0 +1,87 @@
+﻿use std::{cmp::max, str::FromStr, vec};
+use std::{cmp::min, io::*};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn swap(v: &mut Vec<i64>, b: usize, e: usize, t: usize) {
+    for k in 0..e - b {
+        let tmp = v[b + k];
+        v[b + k] = v[t + k];
+        v[t + k] = tmp;
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    sc.new_line();
+    let n = sc.get::<usize>();
+    sc.new_line();
+    let mut v = sc.get_as_vec::<i64>();
+    sc.new_line();
+    let q = sc.get::<usize>();
+
+    for _ in 0..q {
+        sc.new_line();
+        let b = sc.get::<usize>();
+        let e = sc.get::<usize>();
+        let t = sc.get::<usize>();
+
+        swap(&mut v, b, e, t);
+    }
+
+    for i in 0..n {
+        if i == 0 {
+            print!("{}", v[i]);
+        } else {
+            print!(" {}", v[i]);
+        }
+    }
+    println!("")
+}
