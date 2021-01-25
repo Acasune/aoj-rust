7161c8fdf26312fe8480936a849b0b5d32bb8bc3
Acasune <tttt4aaaa@gmail.com>
2021-01-26 08:23:48 +0900

    [Solve] itp2-9-b

---
+++ ITP2/ITP2-9/itp2-9-b.rs
@@ -0,0 +1,80 @@
+﻿use std::collections::Bound::Included;
+use std::{collections::BTreeSet, io::*, str::FromStr};
+
+#[allow(unused_macros)]
+macro_rules! scan {
+  ($e:expr; $t:ty) => {
+    $e.get::<$t>()
+  };
+  ($e:expr; $($t:ty), *) => {
+    ($($e.get::<$t>(),)*)
+  }
+}
+
+struct Scanner<R: BufRead> {
+    reader: R,
+    iter: std::vec::IntoIter<String>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Scanner<R> {
+        Scanner {
+            reader,
+            iter: vec![String::new()].into_iter(),
+        }
+    }
+    fn new_line(&mut self) {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        self.iter = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .into_iter();
+    }
+    fn get<T: FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+    fn get_as_vec<T: FromStr>(&mut self) -> Vec<T> {
+        self.iter.clone().map(|v| v.parse().ok().unwrap()).collect()
+    }
+    fn get_line(&mut self) -> String {
+        let mut line = String::new();
+        self.reader.read_line(&mut line).ok();
+        line.trim().to_string()
+    }
+}
+
+fn main() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let mut tmp: BTreeSet<i64> = BTreeSet::new();
+    let mut bts: BTreeSet<i64> = BTreeSet::new();
+
+    sc.new_line();
+    sc.new_line();
+    let v = sc.get_as_vec::<i64>();
+    for e in v {
+        tmp.insert(e.clone());
+    }
+
+    sc.new_line();
+    sc.new_line();
+    let v = sc.get_as_vec::<i64>();
+    for e in v {
+        match tmp.get(&e) {
+            Some(x) => {
+                let b = bts.insert(x.clone());
+            }
+            None => {}
+        }
+    }
+
+    for e in bts {
+        println!("{}", &e);
+    }
+}
